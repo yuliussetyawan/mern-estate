bb0b2dc09614d742e5c435e800ec258e73a5aa6d
yuliussetyawan <yuliusetyawan@gmail.com>
2024-01-06 23:01:49 +0700

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// mongodb will automatically add 's' if there is more than user, so it's going to be 'users'
+const User = mongoose.model("User", userSchema);
+
+export default User;
