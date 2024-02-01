d47d137e5cf115b9964b93cbd78d6cbc398d1730
yuliussetyawan <yuliusetyawan@gmail.com>
2024-02-01 14:46:49 +0700

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from './routes/listing.route.js';
 // npm install cookie-parser
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -18,23 +19,24 @@ mongoose
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(cookieParser);
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // handle error middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.json({
     success: false,
     statusCode,
-    message
-  })
-})
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
